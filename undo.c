7b7eebebf3db9d359fca64810589adb3b8e161c1
Tom Wright <tom_wright@fastmail.fm>
2015-07-21 23:24:15 -0400

    Add initial support for redo
    Still a couple bugs to work out.

--- undo.c
+++ undo.c
@@ -8,6 +8,12 @@
 #include "undo.h"
 #include "buffer.h"
 
+#if 0
+#define INIT_UNDO_SZ 8192
+#else
+#define INIT_UNDO_SZ 1
+#endif
+
 enum action {
 	INSERT,
 	DELETE
@@ -21,27 +27,33 @@ struct step {
 	char *text;
 };
 
-static unsigned s; /* current step number */
-static struct step *u, *uh; /* undo step struct list, and head of list */
-static int usz, ua;
+static unsigned us, rs; /* current step number */
+/* undo / redo step struct list, and head of list */
+static struct step *u, *uh, *r, *rh;
+static unsigned ua, ra; /* allocated size */
 
-static int checku(void);
+static int checkalloc(struct step **l, struct step **h, unsigned *a);
 static void undosingle(void);
+static void resetr(void);
+static int storeins(struct step **l, struct step **h, unsigned *a,
+                    unsigned s, char *start, char *end);
+static int storedel(struct step **l, struct step **h, unsigned *a,
+                    unsigned s, char *start, char *end);
 
-/* Creates u if needed and ensures u has room for more items. */
-static int checku()
+/* Creates undo/redo list if needed, and ensures space for items. */
+static int checkalloc(struct step **l, struct step **h, unsigned *a)
 {
-	struct step *newu;
-	if (!u) {
-		if (!(u = malloc(sizeof(*u) * 8192)))
+	struct step *new;
+	if (!*l || *h == *l + *a) {
+		if (*a > 0) {
+			*a = *a * 2;
+		} else {
+			*a = INIT_UNDO_SZ;
+		}
+		new = reallocarray(*l, *a, sizeof(**l));
+		if (!new)
 			return -1;
-		ua = 8192;
-	}
-	if (usz == ua) {
-		if (!(newu = realloc(u, ua * 2)))
-			return -1;
-		u = newu;
-		ua *= 2;
+		*l = new;
 	}
 	return 0;
 }
@@ -53,11 +65,13 @@ static void undosingle()
 	st = getbufstart();
 	switch (uh->a) {
 	case INSERT:
+		storedel(&r, &rh, &ra, rs, st + uh->start, st + uh->end);
 		bufdelete(st + uh->start, st + uh->end);
 		break;
 	case DELETE:
 		tsz = uh->end - uh->start;
 		bufinsertstr(uh->text, uh->text + tsz, st + uh->start);
+		storeins(&r, &rh, &ra, rs, st + uh->start, st + uh->end);
 		break;
 	}
 	free(uh->text);
@@ -65,59 +79,117 @@ static void undosingle()
 	uh--;
 }
 
-int recinsert(char *start, char *end)
+static void redosingle()
+{
+	char *st;
+	unsigned tsz;
+	st = getbufstart();
+	switch (rh->a) {
+	case INSERT:
+		storedel(&u, &uh, &ua, us, st + rh->start, st + rh->end);
+		bufdelete(st + rh->start, st + rh->end);
+		break;
+	case DELETE:
+		tsz = rh->end - rh->start;
+		bufinsertstr(rh->text, rh->text + tsz, st + rh->start);
+		storeins(&u, &uh, &ua, us, st + rh->start, st + rh->end);
+		break;
+	}
+	free(rh->text);
+	rh->text = NULL;
+	rh--;
+}
+
+static void resetr()
+{
+	while (rh && rh >= r) {
+		free(rh->text);
+		rh->text = NULL;
+		rh--;
+	}
+	rs = 0;
+}
+
+static int storeins(struct step **l, struct step **h, unsigned *a,
+                    unsigned s, char *start, char *end)
 {
 	assert(inbuf(start) && inbuf(end));
-	if (checku() < 0)
+	if (checkalloc(l, h, a) < 0)
 		return -1;
-	if (!uh)
-		uh = u;
+	if (!*h)
+		*h = *l;
 	else
-		uh++;
-	uh->a = INSERT;
-	uh->s = s;
-	uh->start = start - getbufstart();
-	uh->end = end - getbufstart();
-	uh->text = NULL;
+		(*h)++;
+	(*h)->a = INSERT;
+	(*h)->s = s;
+	(*h)->start = start - getbufstart();
+	(*h)->end = end - getbufstart();
+	(*h)->text = NULL;
+	return 0;
+}
+
+static int storedel(struct step **l, struct step **h, unsigned *a,
+                    unsigned s, char *start, char *end)
+{
+	assert(inbuf(start) && inbuf(end));
+	if (checkalloc(l, h, a) < 0)
+		return -1;
+	if (!*h)
+		*h = *l;
+	else
+		(*h)++;
+	(*h)->a = DELETE;
+	(*h)->s = s;
+	(*h)->start = start - getbufstart();
+	(*h)->end = end - getbufstart();
+	(*h)->text = malloc(end - start);
+	if (!(*h)->text) {
+		(*h)--;
+		return -1;
+	}
+	strncpy((*h)->text, start, end - start);
+	return 0;
+}
+
+int recinsert(char *start, char *end)
+{
+	if (storeins(&u, &uh, &ua, us, start, end) < 0)
+		return -1;
+	resetr();
 	return 0;
 }
 
 int recdelete(char *start, char *end)
 {
-	assert(inbuf(start) && inbuf(end));
-	if (checku() < 0)
+	if (storedel(&u, &uh, &ua, us, start, end) < 0)
 		return -1;
-	if (!uh)
-		uh = u;
-	else
-		uh++;
-	uh->a = DELETE;
-	uh->s = s;
-	uh->start = start - getbufstart();
-	uh->end = end - getbufstart();
-	if (!(uh->text = malloc(end - start))) {
-		if (uh > u)
-			uh--;
-		return -1;
-	}
-	strncpy(uh->text, start, end - start);
+	resetr();
 	return 0;
 }
 
 void recstep()
 {
 	/* Checks the step of the head; don't record empty steps. */
-	if (uh && uh->s == s)
-		s++;
+	if (uh && uh->s == us)
+		us++;
 }
 
 void undo()
 {
-	if (s > 0)
-		s--;
-	while (uh && uh >= u && uh->s >= s)
+	if (us == 0)
+		return;
+	us--;
+	while (uh && uh >= u && uh->s >= us)
 		undosingle();
+	rs++;
 }
 
 void redo()
-{}
+{
+	if (rs == 0)
+		return;
+	rs--;
+	while (rh && rh >= r && rh->s >= rs)
+		redosingle();
+	us++;
+}
