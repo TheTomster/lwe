d69c771cac6f7975138c82ca315cc401c658f297
Tom Wright <tom_wright@fastmail.fm>
2015-08-03 21:43:52 -0400

    BSD_SOURCE and bsd-compat for flock
    This function should be supported just about everywhere if the appropriate macros are defined.

--- config.mk
+++ config.mk
@@ -1,6 +1,9 @@
 LIBS = -lcurses
+# bsd-compat may be needed on non-bsd systems, if you get link errors
+# for `flock`.
+#LIBS += -lbsd-compat
 
-CFLAGS += -g -std=c99 -pedantic -Wall -Wextra -Os -D_XOPEN_SOURCE=700
+CFLAGS += -g -std=c99 -pedantic -Wall -Wextra -Os -D_BSD_SOURCE
 LDFLAGS += -g ${LIBS}
 
 # CC = cc
